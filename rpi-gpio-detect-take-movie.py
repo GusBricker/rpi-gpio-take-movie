a888914a6836f16178b0282f6b5ec42d397d8fb4
Chris Lapa <chris@lapa.com.au>
2015-10-06 08:45:30 +1100

    Implemented timestamped videos, pre/post trigger buffers, threading and more.
    - Videos are stored timestamped under the video_path given.
      They are automagically cleaned up as well.
    - Capturing, process and sending are all done on seperate threads.
    - Videos are captured at the correct framerate.
    - Pre trigger buffer will store x seconds before a trigger event.
    - Added debug flag to show frame, threshold frame, and frame difference.
    
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- rpi-gpio-detect-take-movie.py
+++ rpi-gpio-detect-take-movie.py
@@ -3,6 +3,8 @@
 import argparse
 import cv2
 import time
+import threading
+import Queue
 from cv2 import *
 import RPi.GPIO as GPIO
 import os
@@ -42,16 +44,55 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # return the resized image
     return resized
 
-def send(pushbullet, video_path, msg):
+def on_send_to_pushbullet(api_key, video_path, msg):
+    print "Initializing Pushbullet"
+    pb = Pushbullet(api_key)
+
     print "Opening Video"
     with open(video_path, "rb") as data:
         print "Uploading Video"
-        file_data = pushbullet.upload_file(data, msg)
+        file_data = pb.upload_file(data, msg)
 
     print "Sending Video"
-    push = pushbullet.push_file(**file_data)
+    push = pb.push_file(**file_data)
 
     print "Pushbullet Done!"
+
+    print "Cleaning up old video!"
+    os.remove(video_path)
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+def generate_filename(extension):
+    return time.strftime("%Y%m%d-%H%M%S") + "." + extension
+
+def on_capture_frames(frame_queue, capture, video_rate):
+    frame_period_secs = (float)(1.0 / video_rate)
+
+    while True:
+        pre_capture_timestamp = current_milli_time()
+        if capture.isOpened():
+            (grabbed, frame) = capture.read()
+        else:
+            raise Exception('Capture device not opened!')
+
+        # If the frame could not be grabbed, then we have reached the end
+        # of the video
+        if not grabbed:
+            print "Frame missing!"
+            continue
+
+        frame_queue.put(frame)
+
+        post_capture_timestamp = current_milli_time()
+
+        sleep_time_secs = (float)((post_capture_timestamp - pre_capture_timestamp) / 1000.0)
+        if frame_period_secs > sleep_time_secs:
+            sleep_time_secs = frame_period_secs - sleep_time_secs
+            time.sleep(sleep_time_secs)
+        else:
+            print "Sleeping skipped, consider lowering framerate: " + str(frame_period_secs) + " < " + str(sleep_time_secs)
+
 
 def on_run(args):
     gpio_active_state = (GPIO.HIGH if args.gpio_active == True else GPIO.LOW)
@@ -61,28 +102,24 @@ def on_run(args):
     video_xres = args.xres
     video_yres = args.yres
     video_rate = args.framerate
-    video_length = args.video_length
-    video_path = args.video_path
+    video_extension = "avi"
+    video_format = 'XVID'
     api_key = args.api_key
-    video_name = args.video_name
     min_area = args.min_area
     detect_size = args.detect_size
     threshold = args.threshold
     blur = args.blur
     rebase_period = args.rebase_period
-    num_frames = video_rate * video_length
+    pre_num_frames = video_rate * args.pre_trigger_video_length
+    post_num_frames = video_rate * args.post_trigger_video_length
+    debug = args.debug
+    frame_stack = []
 
-
-    if not os.path.exists(video_path):
-        os.makedirs(video_path)
-    elif not os.path.isdir(video_path):
-        print "video_path must be a directory!"
+    if not os.path.exists(args.video_path):
+        os.makedirs(args.video_path)
+    elif not os.path.isdir(args.video_path):
+        print "args.video_path must be a directory!"
         exit(1)
-
-    video_path = os.path.join(video_path, "output.avi")
-
-    print "Initializing Pushbullet"
-    pb = Pushbullet(api_key)
 
     print "Initializing GPIO"
     GPIO.setmode(GPIO.BCM)
@@ -93,18 +130,31 @@ def on_run(args):
     detected = False
     count = 1
     capture = cv2.VideoCapture(video_cap_dev)
-    format = cv2.cv.CV_FOURCC(*'XVID')
+    format = cv2.cv.CV_FOURCC(*video_format)
     last_rebase_timestamp = time.time()
+
+    frame_queue = Queue.Queue()
+    t = threading.Thread(target=on_capture_frames, args = (frame_queue, capture, video_rate))
+    t.daemon = True
+    t.start()
 
     print "Running"
     try:
         while True:
-            (grabbed, frame) = capture.read()
+            # Detected via gpio activation
+            if not detected:
+                if GPIO.input(gpio_channel) == gpio_active_state:
+                    count = 1
+                    detected = True
+                    video_name = generate_filename(video_extension)
+                    video_path = os.path.join(args.video_path, video_name)
+                    video_writer = cv2.VideoWriter(video_path, format, video_rate, (video_xres, video_yres))
+                    print "GPIO Activated: " + video_path
 
-            # If the frame could not be grabbed, then we have reached the end
-            # of the video
-            if not grabbed:
-                break
+            try:
+                frame = frame_queue.get(block=True, timeout=0.01)
+            except:
+                continue
 
             # Resize the frame, convert it to grayscale, and blur it
             frame = resize(frame, width=detect_size)
@@ -130,6 +180,11 @@ def on_run(args):
             (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                 cv2.CHAIN_APPROX_SIMPLE)
 
+            if debug == True:
+                cv2.imshow("Security Feed", frame)
+                cv2.imshow("Thresh", thresh)
+                cv2.imshow("Frame Delta", frameDelta)
+
             # Loop over the contours
             for c in cnts:
                 # If the contour is too small, ignore it
@@ -145,29 +200,30 @@ def on_run(args):
                 if not detected:
                     count = 1
                     detected = True
-                    print "Object Detected!"
+                    video_name = generate_filename(video_extension)
+                    video_path = os.path.join(args.video_path, video_name)
                     video_writer = cv2.VideoWriter(video_path, format, video_rate, (video_xres, video_yres))
+                    print "Object Detected: " + video_path
 
-            # Detected via gpio activation
-            if not detected:
-                if GPIO.input(gpio_channel) == gpio_active_state:
-                    print "GPIO Activated!"
-                    count = 1
-                    detected = True
-                    video_writer = cv2.VideoWriter(video_path, format, video_rate, (video_xres, video_yres))
+            frame_stack.append(frame)
+            print "frame_stack length: " + str(len(frame_stack))
 
             # Once detected, record into a video stream and send via Pushbullet
             if detected == True:
-                if capture.isOpened():
-                    video_writer.write(frame)
+                if len(frame_stack) > 0:
+                    video_writer.write(frame_stack.pop())
 
                     count += 1
-                    if count > num_frames:
+                    if count > (pre_num_frames + post_num_frames):
                         detected = False
 
                         video_writer = None
 
-                        send(pb, video_path, video_name)
+                        t = threading.Thread(target=on_send_to_pushbullet, args = (api_key, video_path, video_name))
+                        t.daemon = True
+                        t.start()
+            elif len(frame_stack) >= pre_num_frames:
+                frame_stack.pop()
 
     except KeyboardInterrupt:
         print "Quitting"
@@ -183,7 +239,8 @@ def on_run(args):
 parser = argparse.ArgumentParser(description="Captures a video from a webcam when a person is detected in the camera's vision or\n"
                                              "a GPIO is triggered on the Raspberry PI.\n"
                                              "Then sends the video to the user via Pushbullet.")
-parser.add_argument('-video_length', help='Length of video to create in seconds.', type=int, default=10, required=False)
+parser.add_argument('-pre_trigger_video_length', help='Length of video to create in seconds before a trigger event.', type=int, default=10, required=False)
+parser.add_argument('-post_trigger_video_length', help='Length of video to create in seconds after a trigger event.', type=int, default=10, required=False)
 parser.add_argument('-framerate', help='Framerate to capture at.', type=int, default=20, required=False)
 parser.add_argument('-xres', help='X resolution to capture at.', type=int, default=640, required=False)
 parser.add_argument('-yres', help='Y resolution to capture at.', type=int, default=480, required=False)
@@ -193,12 +250,12 @@ parser.add_argument('-gpio_num', help='GPIO channel (BCM mode) to wait for.', ty
 parser.add_argument('-gpio_active_high', help='Active high for when the capture should begin.', dest='gpio_active', required=False, action='store_true')
 parser.add_argument('-gpio_active_low', help='Active low for when the capture should begin.', dest='gpio_active', required=False, action='store_false')
 parser.add_argument('-video_path', help='Local path to save videos.', required=True)
-parser.add_argument('-video_name', help='Name to give video when sending to pushbullet.', required=True)
 parser.add_argument('-min_area', help='Minimum area to detect.', type=int, required=True)
 parser.add_argument('-detect_size', help='Size to shrink frames down before running comparing them. This is a speed optimization.', type=int, required=True)
 parser.add_argument('-threshold', help='Threshold to a person.', type=int, required=True)
 parser.add_argument('-blur', help='Size of blur to apply to each frame.', type=int, required=True)
 parser.add_argument('-rebase_period', help='Frequency in seconds to update our base frame (providing the system isnt in the middle of a detection).', type=int, required=True)
+parser.add_argument('-debug', help='Enable debugging.', required=False, action='store_true')
 parser.set_defaults(gpio_active=False)
 parser.set_defaults(func=on_run)
 args = parser.parse_args()
